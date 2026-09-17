c5fb71ae60c2978ff18e5aa2d23fc9f14364765d
RokiaAlaa <rokiaalaa666@gmail.com>
2026-09-18 00:47:45 +0300

    Prepare v1.2.0 for production

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from CI/CD pipeline! (v1.1.1 webhook test)"
+    return "Hello from CI/CD pipeline! (v1.2.0 prod release)"
 
 @app.route('/health')
 def health():
